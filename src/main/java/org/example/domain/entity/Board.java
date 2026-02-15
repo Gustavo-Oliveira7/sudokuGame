5cf8d4008632407e80ef8fa437fc7088e8bc7170
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2026-02-15 10:55:39 -0300

    protect fixed cells and refactor Board

--- src/main/java/org/example/domain/entity/Board.java
+++ src/main/java/org/example/domain/entity/Board.java
@@ -1,28 +1,47 @@
 package org.example.domain.entity;
 
+
 public class Board {
     private final Cell[][] grid;
 
-    public Board(Cell[][] grid) {
-        if (grid.length != 9) {
+    public Board(int[][] initialValues) {
+        if (initialValues.length != 9) {
             throw new IllegalArgumentException("Tabuleiro inválido");
         }
-        this.grid = grid;
+        this.grid = new Cell[9][9];
+        for (int row = 0; row < 9; row++) {
+            if (initialValues[row].length != 9) {
+                throw new IllegalArgumentException("Cada linha deve ter 9 colunas.");
+            }
+            for (int column = 0; column < 9; column++) {
+                int value = initialValues[row][column];
+                if (value == 0) {
+                    grid[row][column] = new Cell(null, false);
+                } else {
+                    grid[row][column] = new Cell(value, true);
+                }
+            }
+        }
     }
 
     public Cell getCell(Position position) {
         return grid[position.row()][position.column()];
     }
 
-    public Integer getValue(Position position) {
-        return getCell(position).getValue();
+    public Integer getValue(int row, int column) {
+        return grid[row][column].getValue();
     }
 
     public void setValue(Position position, Integer value) {
-        getCell(position).setValue(value);
+        grid[position.row()][position.column()].setValue(value);
     }
 
     public Cell[][] getGrid() {
         return grid;
     }
+
+    public boolean isCellFixed(Position position) {
+        return grid[position.row()][position.column()].isFixed();
+    }
+
 }
